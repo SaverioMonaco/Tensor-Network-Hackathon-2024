333e46044225d8b1383da37fb008a730f787bb6e
Saverio Monaco <saveriomonaco97@gmail.com>
2024-05-20 17:44:52 +0200

    added qubo folder

---
+++ tn_knapsack_optimization/src/qubo.py
@@ -0,0 +1,56 @@
+"""Computation of the hamiltonian matrix
+
+We obtain the following classical many-body Ising-like Hamiltonian:
+
+H = Cte * 1 + h_k \sigma_k + j_kk' \sigma_k\sigma_k'
+"""
+
+# %%
+import numpy as np 
+from typing import List
+
+def get_Q(weights : List[int], profits : List[int], C : int, l : float):
+    assert len(weights) == len(profits)
+    
+    N = len(weights) 
+    M = int(np.floor(np.log2(C)))
+
+    nq = N + M + 1
+
+    Q = np.zeros((nq, nq))
+
+    # Diagonal terms
+    for n in range(N):
+        Q[n,n] += l*(weights[n]**2) - profits[n]
+
+    for m in range(M):
+        Q[N+m, N+m] += 2**(2*m)
+
+    Q[N+M, N+M] += l*(C + 1 + 2**M)**2
+
+    # Off diagonal terms
+    # xj xj' 
+    for n1 in range(N):
+        for n2 in range(n1):
+            Q[n1, n2] += 2*l*weights[n1]*weights[n2]
+            Q[n2, n1]  = Q[n1, n2]
+    # ba ba'
+    for a1 in range(M):
+        for a2 in range(a1):
+            Q[N+a1, N+a2] += l*2**(a1 + a2 + 1)
+            Q[N+a2, N+a1]  = Q[N+a1, N+a2]
+    # xj ba 
+    for n in range(N):
+        for a in range(M):
+            Q[n, N+a] += - l * (2**(a + 1))
+            Q[N+a, n]  = Q[n, N+a]
+    # xj bM
+    for n in range(N):
+        Q[n, N+M] += - 2*l*(C + 1 - 2**M) * weights[n]
+        Q[n, N+M]  = Q[N+M, n]
+    # ba bM
+    for a in range(M):
+        Q[N+a, N+M] += 2*l*(C + 1 - 2**M) * (2**a)
+        Q[N+M, N+a]  = Q[N+a, N+M]
+
+    return Q
