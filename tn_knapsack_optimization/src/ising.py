21bf112f5c5e6723c349066e7df3abb8bde4ac1f
Saverio Monaco <saveriomonaco97@gmail.com>
2024-05-21 17:04:04 +0200

    added scipy implementation

--- tn_knapsack_optimization/src/ising.py
+++ tn_knapsack_optimization/src/ising.py
@@ -1,27 +1,34 @@
+# %% 
 import numpy as np
+import scipy as sp
 
 from qiskit.quantum_info import Pauli, SparsePauliOp
+from typing import List
+from numpy.typing import NDArray
 
 def qubo_to_hamiltonian(qubo):
-    n = qubo.shape[0]
+    nq = qubo.shape[0]
 
     h = {}
     J = {}
 
     # generate the h_k and J_k_k' values
-    for k in range(n):
-        h_k_sum = sum(qubo[k, i] for i in range(k + 1, n))
+    for k in range(nq):
+        h_k_sum = sum(qubo[k, i] for i in range(k + 1, nq))
         h[k] = 0.5 * qubo[k, k] + 0.25 * h_k_sum
-        for j in range(k + 1, n):
+        for j in range(k + 1, nq):
             J[(k, j)] = 0.25 * qubo[k, j]
 
-    Q_kk = sum(qubo[k, k] for k in range(n))
-    Q_kk_prime = sum(qubo[k, k_prime] for k in range(n) for k_prime in range(k + 1, n))
+    # Q_kk = sum(qubo[k, k] for k in range(n))
+    # Q_kk_prime = sum(qubo[k, k_prime] for k in range(n) for k_prime in range(k + 1, n))
+    Q_kk = np.sum(np.diag(qubo))
+    Q_kk_prime = np.sum(np.triu(qubo, 1))
     Cte = 0.5 * Q_kk + 0.25 * Q_kk_prime
 
     return h, J, Cte
 
-def construct_quantum_hamiltonian(h, J, Cte):
+def construct_quantum_hamiltonian_qiskit(h, J, Cte):
+    # ⚠️ QISKIT USES LITTLE ENDIANNESS >:(
     n = len(h)
     pauli_list = []
     coeffs = []
@@ -45,5 +52,34 @@ def construct_quantum_hamiltonian(h, J, Cte):
     H = SparsePauliOp(pauli_list, coeffs)
     return H
 
+def construct_quantum_hamiltonian_scipy(h, J, Cte):
+    n = len(h)
+
+    print(n)
+
+    I = np.array([[1,0], [0,1]])
+
+    def create_operator(ops : List[NDArray], indices : List[int],  n_qubits : int):
+        result = 1
+
+        for i in range(n_qubits):
+            if i in indices:
+                op = ops[indices.index(i)]
+            else:
+                op = I
+
+            result = sp.sparse.kron(result, op)
+
+        return result
+        
+    H = sp.sparse.diags([Cte] * (2**n))
+
+    Z = np.array([[1,0], [0,-1]])
+    for i in range(n):
+        H += h[i]*create_operator([Z], [i], n)
+    for (i, j), J_ij in J.items():
+        H += J_ij*create_operator([Z,Z], [i,j], n)
+    return H 
 
 
+# %%
