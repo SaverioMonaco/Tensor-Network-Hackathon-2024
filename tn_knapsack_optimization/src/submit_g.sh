58ba70e6df071a5d1d14e1f82c5ee5e80c5c40a9
gpierini00 <gabrypierini@libero.it>
2024-05-23 12:05:51 +0200

    EO

--- tn_knapsack_optimization/src/submit_g.sh
+++ tn_knapsack_optimization/src/submit_g.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#SBATCH -N 1
-#SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:1
-#SBATCH -p g100_usr_interactive 
+#SBATCH -A tra24_qc_week_0
+#SBATCH -p g100_usr_interactive
 #SBATCH -t 30:00
 
 python3 qaoa_qmatcha_run_latest.py
